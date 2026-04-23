d4d2491d36e2f1d38a654caf783928c98822501f
Laura Alexandra Herrera Espitia <est.lauraa.herrerae@unimilitar.edu.co>
2026-04-23 11:12:30 -0500

    se adjunto el documento de texto y la imagen

--- src/main/java/com/mycompany/arbolbinario/ArbolBinario.java
+++ src/main/java/com/mycompany/arbolbinario/ArbolBinario.java
@@ -1,16 +1,66 @@
 
 package com.mycompany.arbolbinario;
+class Nodo {
+    int valor;
+    Nodo izq, der;
 
+    public Nodo(int valor) {
+        this.valor = valor;
+        izq = der = null;
+    }
+}
+
+class Arbol {
+    Nodo raiz;
+
+    public Nodo insertar(Nodo raiz, int valor) {
+        if (raiz == null) return new Nodo(valor);
+
+        if (valor < raiz.valor)
+            raiz.izq = insertar(raiz.izq, valor);
+        else
+            raiz.der = insertar(raiz.der, valor);
+
+        return raiz;
+    }
+
+    // INORDEN
+    public void inorden(Nodo n) {
+        if (n != null) {
+            inorden(n.izq);
+            System.out.print(n.valor + " ");
+            inorden(n.der);
+        }
+    }
+
+    // PREORDEN
+    public void preorden(Nodo n) {
+        if (n != null) {
+            System.out.print(n.valor + " ");
+            preorden(n.izq);
+            preorden(n.der);
+        }
+    }
+
+    // POSTORDEN
+    public void postorden(Nodo n) {
+        if (n != null) {
+            postorden(n.izq);
+            postorden(n.der);
+            System.out.print(n.valor + " ");
+        }
+    }
+}
 
 public class ArbolBinario {
 
-    public static void main(String[] args) {
-     Arbol a = new Arbol();
+     public static void main(String[] args) {
+        Arbol a = new Arbol();
 
-        int[] valores = {50, 30, 70, 20, 40, 60, 80};
+        int[] datos = {40, 20, 60, 10, 30};
 
-        for (int v : valores)
-            a.raiz = a.insertar(a.raiz, v);
+        for (int x : datos)
+            a.raiz = a.insertar(a.raiz, x);
 
         System.out.print("Inorden: ");
         a.inorden(a.raiz);
@@ -22,3 +72,4 @@ public class ArbolBinario {
         a.postorden(a.raiz);
     }
 }
+
